6af1094891d39e53f26306b61e3f236dd5c2b9a5
Paul Solt <PaulSolt@gmail.com>
2018-12-08 17:32:58 -0500

    Initial Commit

---
+++ Mac Trial/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Mac Trial
+//
+//  Created by Paul Solt on 12/8/18.
+//  Copyright © 2018 Paul Solt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
